0f8ccb830848219173e5f50f48be874012ce5b36
sihamEgb <siham.egb@gmail.com>
2015-01-14 22:21:19 +0200

    meeting entity

--- src/technion/bookclub/entities/Meeting.java
+++ src/technion/bookclub/entities/Meeting.java
@@ -1,26 +1,37 @@
 package technion.bookclub.entities;
 
-import java.util.Set;
+import com.google.gson.Gson;
 
 public class Meeting {
-	private String book;
-	private String date;
-	private Set<String> participants;
 
-	public String getBook() {
-		return book;
+	// private String book;
+	// private String date;
+	// private Set<String> participants;
+
+	private String meetingId;
+	private String title;
+	private String location;
+	private String clubId;
+
+	/*
+	 * public String getBook() { return book; }
+	 * 
+	 * public String getDate() { return date; }
+	 * 
+	 * public Integer getParticipantsNumber() { return participants.size(); }
+	 * 
+	 * public void addParticipant(String participant) {
+	 * participants.add(participant); }
+	 */
+	public String toJson() {
+		Gson gson = new Gson();
+		String json = gson.toJson(this);
+		return json;
 	}
 
-	public String getDate() {
-		return date;
-	}
-
-	public Integer getParticipantsNumber() {
-		return participants.size();
-	}
-
-	public void addParticipant(String participant) {
-		participants.add(participant);
+	public static Meeting constructFromJson(String json) {
+		Gson gson = new Gson();
+		return gson.fromJson(json, Meeting.class);
 	}
 
 }
