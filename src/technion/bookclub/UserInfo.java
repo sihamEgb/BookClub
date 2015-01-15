dda081305acade91cc9b76ffc56987dc5240601b
sihamEgb <siham.egb@gmail.com>
2015-01-15 10:43:54 +0200

    ss

--- src/technion/bookclub/UserInfo.java
+++ src/technion/bookclub/UserInfo.java
@@ -1,10 +1,16 @@
 package technion.bookclub;
 
 public class UserInfo {
-	private static String id = "4859722209427456";
-	
+	private static String id = "5222194095325184";//"5278093363118080";
+	//String id = "4859722209427456";
+	// Fatima 5222194095325184
+	// Ekram 6583832140578816
+	// Siham 4859722209427456
+	// Salam 5278093363118080
+	// Bob 4824175986343936
 	public static String getId(){
 		return id;
+		
 	}
 
 }
